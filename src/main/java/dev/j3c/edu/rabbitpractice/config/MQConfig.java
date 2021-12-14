118dc4a896ca58f367b03d3b844e45f8f9742230
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-12-14 14:00:54 -0500

    Changed the server port to 9292

--- src/main/java/dev/j3c/edu/rabbitpractice/config/MQConfig.java
+++ src/main/java/dev/j3c/edu/rabbitpractice/config/MQConfig.java
@@ -12,9 +12,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MQConfig {
 
-    static final String QUEUE = "message_queue";
-    static final String EXCHANGE = "message_exchange";
-    static final String ROUTING_KEY = "message_routingKey";
+    public static final String QUEUE = "message_queue";
+    public static final String EXCHANGE = "message_exchange";
+    public static final String ROUTING_KEY = "message_routingKey";
 
     @Bean
     public Queue queue(){
